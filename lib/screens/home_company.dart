5904c9ca1fb894dec57863a7f5cabd244bc4f3eb
Joshuarupasinghe <joshuajrupasinghe@gmail.com>
2025-04-03 19:55:08 +0530

    Revert "Merge pull request #168 from Joshuarupasinghe/MEM-sachintha"
    This reverts commit ff0601eb00fad3f14760e6a02b15a0ea3a7640b5, reversing
    changes made to 19f613789e63d6fd569a01684b0809c4fa391c9e.

--- lib/screens/home_company.dart
+++ lib/screens/home_company.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:bio_boost/screens/AgriWasteType.dart';
 import 'package:flutter/material.dart';
 import 'package:bio_boost/services/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
